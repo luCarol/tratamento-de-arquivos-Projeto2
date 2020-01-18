89b27e3c07a991320c65f4ebe50fa36b0b989cf3
Luana <lucarolp.deoli@gmail.com>
2020-01-18 13:51:14 -0300

    Bloco try-with-resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,39 +20,18 @@ public class Program {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        String path = "c:\\temp\\in.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
-        
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-            
+
+        String path = "c:\\temp\\in.txt ";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
-            
+
             while (line != null) {
                 System.out.println(line);
                 line = br.readLine();
             }
-            
         } catch (IOException e) {
-            System.out.println("Error: " +e.getMessage());
+            System.out.println("Error: " + e.getMessage());
         }
-        finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            
-        }
-        
     }
-    
 }
